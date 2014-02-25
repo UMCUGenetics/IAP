d65a1a7c880d8971586d12dbdebd5c6c15c20d4d
Robert Ernst <r.f.ernst-3@umcutrecht.nl>
2014-02-25 15:11:55 +0000

    small fix

--- QScripts/IndelRealigner.scala
+++ QScripts/IndelRealigner.scala
@@ -23,7 +23,7 @@
 * THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-package org.broadinstitute.sting.queue.qscripts.examples
+package org.broadinstitute.sting.queue.qscripts
 
 import org.broadinstitute.sting.queue.QScript
 import org.broadinstitute.sting.queue.extensions.gatk._
