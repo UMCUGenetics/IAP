9b7da4d3392a37b03ac33f734f61990137d0d1f8
rernst <r.f.ernst-3@umcutrecht.nl>
2015-02-26 12:59:33 +0100

    Add more checks before and after running somatic caller jobs

--- illumina_somaticVariants.pm
+++ illumina_somaticVariants.pm
@@ -138,8 +138,11 @@ sub runSomaticVariantCallers {
 		print MERGE_SH "\njava -Xmx6G -jar $opt{GATK_PATH}/GenomeAnalysisTK.jar -T SelectVariants -R $opt{GENOME} -L $opt{SOMVAR_TARGETS} -V $sample_tumor_out_dir/$sample_tumor_name\_merged_somatics.vcf -o $sample_tumor_out_dir/$sample_tumor_name\_filtered_merged_somatics.vcf\n";
 		print MERGE_SH "rm $sample_tumor_out_dir/$sample_tumor_name\_merged_somatics.vcf*";
 	    }
-	    print MERGE_SH "\n\ntouch $sample_tumor_log_dir/$sample_tumor_name.done\n\n";
-
+	    
+	    print MERGE_SH "\n\nif [ -f $sample_tumor_out_dir/$sample_tumor_name\_merged_somatics.vcf -o -f $sample_tumor_out_dir/$sample_tumor_name\_filtered_merged_somatics.vcf ]\n";
+	    print MERGE_SH "then\n";
+	    print MERGE_SH "\ttouch $sample_tumor_log_dir/$sample_tumor_name.done\n";
+	    print MERGE_SH "fi\n";
 	    print MERGE_SH "echo \"END Merge\t\" `date` `uname -n` >> $sample_tumor_log_dir/merge.log\n\n";
 	    close MERGE_SH;
 
@@ -174,23 +177,30 @@ sub runStrelka {
 
     open STRELKA_SH, ">$bash_file" or die "cannot open file $bash_file \n";
     print STRELKA_SH "#!/bin/bash\n\n";
-    print STRELKA_SH "echo \"Start Strelka\t\" `date` \"\t $sample_ref_bam \t $sample_tumor_bam\t\" `uname -n` >> $log_dir/strelka.log\n\n";
+    print STRELKA_SH "if [ -f $sample_tumor_bam -a -f $sample_ref_bam ]\n";
+    print STRELKA_SH "then\n";
+    print STRELKA_SH "\techo \"Start Strelka\t\" `date` \"\t $sample_ref_bam \t $sample_tumor_bam\t\" `uname -n` >> $log_dir/strelka.log\n\n";
 
     # Run Strelka
-    print STRELKA_SH "$opt{STRELKA_PATH}/bin/configureStrelkaWorkflow.pl --tumor $sample_tumor_bam --normal $sample_ref_bam --ref $opt{GENOME} --config $opt{STRELKA_INI} --output-dir $strelka_out_dir\n\n";
+    print STRELKA_SH "\t$opt{STRELKA_PATH}/bin/configureStrelkaWorkflow.pl --tumor $sample_tumor_bam --normal $sample_ref_bam --ref $opt{GENOME} --config $opt{STRELKA_INI} --output-dir $strelka_out_dir\n\n";
 
-    print STRELKA_SH "cd $strelka_out_dir\n";
-    print STRELKA_SH "make -j 8\n\n";
+    print STRELKA_SH "\tcd $strelka_out_dir\n";
+    print STRELKA_SH "\tmake -j 8\n\n";
 
     # Check strelka completed
-    print STRELKA_SH "if [ -f $strelka_out_dir/task.complete ]\n";
-    print STRELKA_SH "then\n";
-    print STRELKA_SH "\tjava -Xmx6G -jar $opt{GATK_PATH}/GenomeAnalysisTK.jar -T CombineVariants -R $opt{GENOME} -o passed.somatic.merged.vcf -V results/passed.somatic.snvs.vcf -V results/passed.somatic.indels.vcf \n";
-    print STRELKA_SH "\tperl -p -e 's/\\t([A-Z][A-Z]:)/\\tGT:\$1/g' passed.somatic.merged.vcf | perl -p -e 's/(:T[UO]R?)\\t/\$1\\t0\\/0:/g' | perl -p -e 's/(:\\d+,\\d+)\\t/\$1\\t0\\/1:/g' | perl -p -e 's/(#CHROM.*)/##StrelkaGATKCompatibility=Added GT fields to strelka calls for gatk compatibility.\\n\$1/g' > temp.vcf\n";
-    print STRELKA_SH "\tmv temp.vcf passed.somatic.merged.vcf\n";
-    print STRELKA_SH "\ttouch $log_dir/strelka.done\n";
-    print STRELKA_SH "fi\n\n";
-    print STRELKA_SH "echo \"End Strelka\t\" `date` \"\t $sample_ref_bam \t $sample_tumor_bam\t\" `uname -n` >> $log_dir/strelka.log\n\n";
+    print STRELKA_SH "\tif [ -f $strelka_out_dir/task.complete ]\n";
+    print STRELKA_SH "\tthen\n";
+    print STRELKA_SH "\t\tjava -Xmx6G -jar $opt{GATK_PATH}/GenomeAnalysisTK.jar -T CombineVariants -R $opt{GENOME} -o passed.somatic.merged.vcf -V results/passed.somatic.snvs.vcf -V results/passed.somatic.indels.vcf \n";
+    print STRELKA_SH "\t\tperl -p -e 's/\\t([A-Z][A-Z]:)/\\tGT:\$1/g' passed.somatic.merged.vcf | perl -p -e 's/(:T[UO]R?)\\t/\$1\\t0\\/0:/g' | perl -p -e 's/(:\\d+,\\d+)\\t/\$1\\t0\\/1:/g' | perl -p -e 's/(#CHROM.*)/##StrelkaGATKCompatibility=Added GT fields to strelka calls for gatk compatibility.\\n\$1/g' > temp.vcf\n";
+    print STRELKA_SH "\t\tmv temp.vcf passed.somatic.merged.vcf\n";
+    print STRELKA_SH "\t\ttouch $log_dir/strelka.done\n";
+    print STRELKA_SH "\tfi\n\n";
+    print STRELKA_SH "\techo \"End Strelka\t\" `date` \"\t $sample_ref_bam \t $sample_tumor_bam\t\" `uname -n` >> $log_dir/strelka.log\n\n";
+    
+    print STRELKA_SH "else\n";
+    print STRELKA_SH "\techo \"ERROR: $sample_tumor_bam or $sample_ref_bam does not exist.\" >&2\n";
+    print STRELKA_SH "fi\n";
+    
     close STRELKA_SH;
 
     ## Run job
@@ -228,37 +238,42 @@ sub runVarscan {
     open VARSCAN_SH, ">$bash_file" or die "cannot open file $bash_file \n";
     print VARSCAN_SH "#!/bin/bash\n\n";
     print VARSCAN_SH "cd $varscan_out_dir\n";
-    print VARSCAN_SH "echo \"Start Pileup\t\" `date` \"$sample_ref_bam \t $sample_tumor_bam\t\" `uname -n` >> $log_dir/varscan.log\n";
-
+    print VARSCAN_SH "if [ -f $sample_tumor_bam -a -f $sample_ref_bam ]\n";
+    print VARSCAN_SH "then\n";
+    
     # run pileups
+    print VARSCAN_SH "\techo \"Start Pileup\t\" `date` \"$sample_ref_bam \t $sample_tumor_bam\t\" `uname -n` >> $log_dir/varscan.log\n";
     if ((!-e "$sample_tumor_bam.pileup")||(-z "$sample_tumor_bam.pileup")) {
-	print VARSCAN_SH "$opt{SAMTOOLS_PATH}/samtools mpileup -q 1 -f $opt{GENOME} -l $opt{SOMVAR_TARGETS} $sample_tumor_bam > $sample_tumor_bam.pileup\n";
+	print VARSCAN_SH "\t$opt{SAMTOOLS_PATH}/samtools mpileup -q 1 -f $opt{GENOME} -l $opt{SOMVAR_TARGETS} $sample_tumor_bam > $sample_tumor_bam.pileup\n";
     }
     if ((!-e "$sample_ref_bam.pileup")||(-z "$sample_ref_bam.pileup")) {
-	print VARSCAN_SH "$opt{SAMTOOLS_PATH}/samtools mpileup -q 1 -f $opt{GENOME} -l $opt{SOMVAR_TARGETS} $sample_ref_bam > $sample_ref_bam.pileup\n";
+	print VARSCAN_SH "\t$opt{SAMTOOLS_PATH}/samtools mpileup -q 1 -f $opt{GENOME} -l $opt{SOMVAR_TARGETS} $sample_ref_bam > $sample_ref_bam.pileup\n";
     }
-    print VARSCAN_SH "echo \"End Pileup\t\" `date` \"\t $sample_ref_bam \t $sample_tumor_bam\t\" `uname -n` >> $log_dir/varscan.log\n\n";
+    print VARSCAN_SH "\techo \"End Pileup\t\" `date` \"\t $sample_ref_bam \t $sample_tumor_bam\t\" `uname -n` >> $log_dir/varscan.log\n\n";
 
     # run varscan
-    print VARSCAN_SH "echo \"Start Varscan\t\" `date` \"\t $sample_ref_bam \t $sample_tumor_bam\t\" `uname -n` >> $log_dir/varscan.log\n";
-    #print VARSCAN_SH "tumor_pileup=\"$opt{SAMTOOLS_PATH}/samtools mpileup -q 1 -f $opt{GENOME} -l $opt{SOMVAR_TARGETS} $sample_tumor_bam\"\n";
-    #print VARSCAN_SH "ref_pileup=\"$opt{SAMTOOLS_PATH}/samtools mpileup -q 1 -f $opt{GENOME} -l $opt{SOMVAR_TARGETS} $sample_ref_bam\"\n";
-    #print VARSCAN_SH "java -Xmx12g -jar $opt{VARSCAN_PATH} somatic <(\$ref_pileup) <(\$tumor_pileup) $sample_tumor_name $opt{VARSCAN_SETTINGS} --output-vcf 1\n\n";
+    print VARSCAN_SH "\techo \"Start Varscan\t\" `date` \"\t $sample_ref_bam \t $sample_tumor_bam\t\" `uname -n` >> $log_dir/varscan.log\n";
+    #print VARSCAN_SH "\ttumor_pileup=\"$opt{SAMTOOLS_PATH}/samtools mpileup -q 1 -f $opt{GENOME} -l $opt{SOMVAR_TARGETS} $sample_tumor_bam\"\n";
+    #print VARSCAN_SH "\tref_pileup=\"$opt{SAMTOOLS_PATH}/samtools mpileup -q 1 -f $opt{GENOME} -l $opt{SOMVAR_TARGETS} $sample_ref_bam\"\n";
+    #print VARSCAN_SH "\tjava -Xmx12g -jar $opt{VARSCAN_PATH} somatic <(\$ref_pileup) <(\$tumor_pileup) $sample_tumor_name $opt{VARSCAN_SETTINGS} --output-vcf 1\n\n";
 
-    print VARSCAN_SH "java -Xmx12g -jar $opt{VARSCAN_PATH} somatic $sample_ref_bam.pileup $sample_tumor_bam.pileup $sample_tumor_name $opt{VARSCAN_SETTINGS} --output-vcf 1\n\n";
+    print VARSCAN_SH "\tjava -Xmx12g -jar $opt{VARSCAN_PATH} somatic $sample_ref_bam.pileup $sample_tumor_bam.pileup $sample_tumor_name $opt{VARSCAN_SETTINGS} --output-vcf 1\n\n";
 
     # postprocessing
-    print VARSCAN_SH "java -Xmx12g -jar $opt{VARSCAN_PATH} processSomatic $sample_tumor_name.indel.vcf $opt{VARSCAN_POSTSETTINGS}\n";
-    print VARSCAN_SH "java -Xmx12g -jar $opt{VARSCAN_PATH} processSomatic $sample_tumor_name.snp.vcf $opt{VARSCAN_POSTSETTINGS}\n\n";
+    print VARSCAN_SH "\tjava -Xmx12g -jar $opt{VARSCAN_PATH} processSomatic $sample_tumor_name.indel.vcf $opt{VARSCAN_POSTSETTINGS}\n";
+    print VARSCAN_SH "\tjava -Xmx12g -jar $opt{VARSCAN_PATH} processSomatic $sample_tumor_name.snp.vcf $opt{VARSCAN_POSTSETTINGS}\n\n";
     
     # merge varscan hc snps and indels
-    print VARSCAN_SH "java -Xmx6G -jar $opt{GATK_PATH}/GenomeAnalysisTK.jar -T CombineVariants -R $opt{GENOME} -o $sample_tumor_name.merged.Somatic.hc.vcf -V $sample_tumor_name.snp.Somatic.hc.vcf -V $sample_tumor_name.indel.Somatic.hc.vcf\n";
-    print VARSCAN_SH "sed -i 's/SSC/VS_SSC/' $sample_tumor_name.merged.Somatic.hc.vcf\n\n"; # to resolve merge conflict with FB vcfs
+    print VARSCAN_SH "\tjava -Xmx6G -jar $opt{GATK_PATH}/GenomeAnalysisTK.jar -T CombineVariants -R $opt{GENOME} -o $sample_tumor_name.merged.Somatic.hc.vcf -V $sample_tumor_name.snp.Somatic.hc.vcf -V $sample_tumor_name.indel.Somatic.hc.vcf\n";
+    print VARSCAN_SH "\tsed -i 's/SSC/VS_SSC/' $sample_tumor_name.merged.Somatic.hc.vcf\n\n"; # to resolve merge conflict with FB vcfs
     
     # Check varscan completed
-    print VARSCAN_SH "touch $log_dir/varscan.done\n\n"; ## Check on complete output!!!
-    print VARSCAN_SH "echo \"End Varscan\t\" `date` \"\t $sample_ref_bam \t $sample_tumor_bam\t\" `uname -n` >> $log_dir/varscan.log\n";
+    print VARSCAN_SH "\ttouch $log_dir/varscan.done\n\n"; ## Check on complete output!!!
+    print VARSCAN_SH "\techo \"End Varscan\t\" `date` \"\t $sample_ref_bam \t $sample_tumor_bam\t\" `uname -n` >> $log_dir/varscan.log\n";
 
+    print VARSCAN_SH "else\n";
+    print VARSCAN_SH "\techo \"ERROR: $sample_tumor_bam or $sample_ref_bam does not exist.\" >&2\n";
+    print VARSCAN_SH "fi\n";
     close VARSCAN_SH;
 
     ## Run job
@@ -295,34 +310,41 @@ sub runFreeBayes {
     # Create bash script
     open FREEBAYES_SH, ">$bash_file" or die "cannot open file $bash_file \n";
     print FREEBAYES_SH "#!/bin/bash\n\n";
-    print FREEBAYES_SH "echo \"Start Freebayes\t\" `date` \"\t $sample_ref_bam \t $sample_tumor_bam\t\" `uname -n` >> $log_dir/freebayes.log\n";
+    print FREEBAYES_SH "if [ -f $sample_tumor_bam -a -f $sample_ref_bam ]\n";
+    print FREEBAYES_SH "then\n";
+    
+    print FREEBAYES_SH "\techo \"Start Freebayes\t\" `date` \"\t $sample_ref_bam \t $sample_tumor_bam\t\" `uname -n` >> $log_dir/freebayes.log\n";
 
     # Run freebayes
-    print FREEBAYES_SH "$opt{FREEBAYES_PATH}/freebayes -f $opt{GENOME} -t $opt{SOMVAR_TARGETS} $opt{FREEBAYES_SETTINGS} $sample_ref_bam $sample_tumor_bam > $freebayes_out_dir/$sample_tumor_name.vcf\n\n";
+    print FREEBAYES_SH "\t$opt{FREEBAYES_PATH}/freebayes -f $opt{GENOME} -t $opt{SOMVAR_TARGETS} $opt{FREEBAYES_SETTINGS} $sample_ref_bam $sample_tumor_bam > $freebayes_out_dir/$sample_tumor_name.vcf\n\n";
 
     # Uniqify freebayes output 
-    print FREEBAYES_SH "uniq $freebayes_out_dir/$sample_tumor_name.vcf > $freebayes_out_dir/$sample_tumor_name.uniq.vcf\n";
-    print FREEBAYES_SH "mv $freebayes_out_dir/$sample_tumor_name.uniq.vcf $freebayes_out_dir/$sample_tumor_name.vcf\n\n";
+    print FREEBAYES_SH "\tuniq $freebayes_out_dir/$sample_tumor_name.vcf > $freebayes_out_dir/$sample_tumor_name.uniq.vcf\n";
+    print FREEBAYES_SH "\tmv $freebayes_out_dir/$sample_tumor_name.uniq.vcf $freebayes_out_dir/$sample_tumor_name.vcf\n\n";
 
     # get sample ids
-    print FREEBAYES_SH "sample_R=`grep -P \"^#CHROM\" $freebayes_out_dir/$sample_tumor_name.vcf | cut -f 10`\n";
-    print FREEBAYES_SH "sample_T=`grep -P \"^#CHROM\" $freebayes_out_dir/$sample_tumor_name.vcf | cut -f 11`\n\n";
+    print FREEBAYES_SH "\tsample_R=`grep -P \"^#CHROM\" $freebayes_out_dir/$sample_tumor_name.vcf | cut -f 10`\n";
+    print FREEBAYES_SH "\tsample_T=`grep -P \"^#CHROM\" $freebayes_out_dir/$sample_tumor_name.vcf | cut -f 11`\n\n";
 
     # annotate somatic and germline scores
-    print FREEBAYES_SH "$opt{VCFSAMPLEDIFF_PATH}/vcfsamplediff VT \$sample_R \$sample_T $freebayes_out_dir/$sample_tumor_name.vcf > $freebayes_out_dir/$sample_tumor_name\_VTannot.vcf\n";
-    print FREEBAYES_SH "sed -i 's/SSC/FB_SSC/' $freebayes_out_dir/$sample_tumor_name\_VTannot.vcf\n"; # to resolve merge conflicts with varscan vcfs
-    print FREEBAYES_SH "grep -P \"^#\" $freebayes_out_dir/$sample_tumor_name\_VTannot.vcf > $freebayes_out_dir/$sample_tumor_name\_germline.vcf\n";
-    print FREEBAYES_SH "grep -P \"^#\" $freebayes_out_dir/$sample_tumor_name\_VTannot.vcf > $freebayes_out_dir/$sample_tumor_name\_somatic.vcf\n";
-    print FREEBAYES_SH "grep -i \"VT=germline\" $freebayes_out_dir/$sample_tumor_name\_VTannot.vcf >> $freebayes_out_dir/$sample_tumor_name\_germline.vcf\n";
-    print FREEBAYES_SH "grep -i \"VT=somatic\" $freebayes_out_dir/$sample_tumor_name\_VTannot.vcf >> $freebayes_out_dir/$sample_tumor_name\_somatic.vcf\n";
-    print FREEBAYES_SH "rm $freebayes_out_dir/$sample_tumor_name\_VTannot.vcf\n\n";
+    print FREEBAYES_SH "\t$opt{VCFSAMPLEDIFF_PATH}/vcfsamplediff VT \$sample_R \$sample_T $freebayes_out_dir/$sample_tumor_name.vcf > $freebayes_out_dir/$sample_tumor_name\_VTannot.vcf\n";
+    print FREEBAYES_SH "\tsed -i 's/SSC/FB_SSC/' $freebayes_out_dir/$sample_tumor_name\_VTannot.vcf\n"; # to resolve merge conflicts with varscan vcfs
+    print FREEBAYES_SH "\tgrep -P \"^#\" $freebayes_out_dir/$sample_tumor_name\_VTannot.vcf > $freebayes_out_dir/$sample_tumor_name\_germline.vcf\n";
+    print FREEBAYES_SH "\tgrep -P \"^#\" $freebayes_out_dir/$sample_tumor_name\_VTannot.vcf > $freebayes_out_dir/$sample_tumor_name\_somatic.vcf\n";
+    print FREEBAYES_SH "\tgrep -i \"VT=germline\" $freebayes_out_dir/$sample_tumor_name\_VTannot.vcf >> $freebayes_out_dir/$sample_tumor_name\_germline.vcf\n";
+    print FREEBAYES_SH "\tgrep -i \"VT=somatic\" $freebayes_out_dir/$sample_tumor_name\_VTannot.vcf >> $freebayes_out_dir/$sample_tumor_name\_somatic.vcf\n";
+    print FREEBAYES_SH "\trm $freebayes_out_dir/$sample_tumor_name\_VTannot.vcf\n\n";
 
     # Filter
-    print FREEBAYES_SH "cat $freebayes_out_dir/$sample_tumor_name\_somatic.vcf | $opt{BIOVCF_PATH}/bio-vcf $opt{FREEBAYES_SOMATICFILTER} > $freebayes_out_dir/$sample_tumor_name\_somatic_filtered.vcf\n";
-    print FREEBAYES_SH "cat $freebayes_out_dir/$sample_tumor_name\_germline.vcf | $opt{BIOVCF_PATH}/bio-vcf $opt{FREEBAYES_GERMLINEFILTER} > $freebayes_out_dir/$sample_tumor_name\_germline_filtered.vcf\n\n";
+    print FREEBAYES_SH "\tcat $freebayes_out_dir/$sample_tumor_name\_somatic.vcf | $opt{BIOVCF_PATH}/bio-vcf $opt{FREEBAYES_SOMATICFILTER} > $freebayes_out_dir/$sample_tumor_name\_somatic_filtered.vcf\n";
+    print FREEBAYES_SH "\tcat $freebayes_out_dir/$sample_tumor_name\_germline.vcf | $opt{BIOVCF_PATH}/bio-vcf $opt{FREEBAYES_GERMLINEFILTER} > $freebayes_out_dir/$sample_tumor_name\_germline_filtered.vcf\n\n";
 
-    print FREEBAYES_SH "touch $log_dir/freebayes.done\n\n"; ## Check on complete output!!!
-    print FREEBAYES_SH "echo \"End Freebayes\t\" `date` \"\t $sample_ref_bam \t $sample_tumor_bam\t\" `uname -n` >> $log_dir/freebayes.log\n";
+    print FREEBAYES_SH "\ttouch $log_dir/freebayes.done\n\n"; ## Check on complete output!!!
+    print FREEBAYES_SH "\techo \"End Freebayes\t\" `date` \"\t $sample_ref_bam \t $sample_tumor_bam\t\" `uname -n` >> $log_dir/freebayes.log\n";
+
+    print FREEBAYES_SH "else\n";
+    print FREEBAYES_SH "\techo \"ERROR: $sample_tumor_bam or $sample_ref_bam does not exist.\" >&2\n";
+    print FREEBAYES_SH "fi\n";
 
     close FREEBAYES_SH;
 
