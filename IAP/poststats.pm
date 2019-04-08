d14fe1efd0eb608cdee7239d1fdc5d7bdbc5d01b
rernst <r.f.ernst-3@umcutrecht.nl>
2019-04-08 10:10:10 +0200

    Add temp directory

--- IAP/poststats.pm
+++ IAP/poststats.pm
@@ -68,6 +68,7 @@ sub runPostStats {
 	
 	my $bashFile = $opt{OUTPUT_DIR}."/jobs/".$jobID.".sh";
 	my $logDir = $opt{OUTPUT_DIR}."/logs";
+	my $tmpDir = $opt{OUTPUT_DIR}."/tmp";
         my $outputDir = "Exoncov";
         
 	open PS_SH, ">$bashFile" or die "cannot open file $bashFile\n";
@@ -86,7 +87,7 @@ sub runPostStats {
 		print PS_SH ". $opt{EXONCOVV3_PATH}/venv/bin/activate\n";
 		foreach my $sample (@{$opt{SAMPLES}}){
 		    my $sampleBam = "$opt{OUTPUT_DIR}/$sample/mapping/$opt{BAM_FILES}->{$sample}";
-		    print PS_SH "python $opt{EXONCOVV3_PATH}/ExonCov.py import_bam --overwrite --exon_bed $opt{EXONCALLCOV_BED} --threads $opt{POSTSTATS_THREADS} $runName $sampleBam > $logDir/ExonCovV3_$sample.log 2> $logDir/ExonCovV3_$sample.log\n";
+		    print PS_SH "python $opt{EXONCOVV3_PATH}/ExonCov.py import_bam --overwrite --exon_bed $opt{EXONCALLCOV_BED} --threads $opt{POSTSTATS_THREADS} --temp $tmpDir $runName $sampleBam > $logDir/ExonCovV3_$sample.log 2> $logDir/ExonCovV3_$sample.log\n";
 		}
 	    }
 	}
